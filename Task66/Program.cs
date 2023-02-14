2040c67d32f5c94d44e94fa37aa619987be4901f
AlexanderAkh <sancheek240@mail.ru>
2023-02-14 15:44:21 +0300

    Add task

---
+++ Task66/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+
+int RecNum(int numA, int numB)
+{
+    if (numB > numA)
+        return numB + RecNum(numA, numB - 1);
+    return numB;
+}
+System.Console.WriteLine(RecNum(4,8));
+
---
+++ Task68/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+
+
+int FuncAkkerm(int numA, int numB)
+{
+    if (numA == 0)
+    {
+        return numB + 1;
+    }
+    if (numA > 0 && numB == 0)
+    {
+        return FuncAkkerm(numA - 1, 1);
+    }
+    if (numA > 0 && numB > 0)
+    {
+        return FuncAkkerm(numA - 1 , FuncAkkerm(numA, numB - 1));
+    }
+    return 0;
+}
+
+System.Console.WriteLine(FuncAkkerm(2, 3));
